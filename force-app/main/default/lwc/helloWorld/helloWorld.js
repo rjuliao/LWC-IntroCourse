99c1b566f3b564a6671fbc2934c29f950f4b85dc
Raul Juliao <raulj@uninorte.edu.co>
2021-04-10 17:09:12 -0500

    Data binding and track

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    //local properties
+    name            //undefined
+    age = 22
+    fullname = "Salesforce Colombia"
+    iitle = 'aura'
+
+    //methods
+    changeHandler(event) {
+        //logic 
+        this.title = event.target.value
+    }
+
+    @track address = {
+        city : 'Bogotá',
+        postCode : 111015,
+        country : 'Colombia' 
+    }
+
+    trackHandler(event){
+        this.address.city = event.target.value
+        //another way to update without using track
+        //this.address = {...this.address, "city": event.target.value}
+    }
+     
+}
