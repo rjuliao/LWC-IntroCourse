2006300d6a3984741b7d0181f9c8cd76cdf4ff43
Raul Juliao <raulj@uninorte.edu.co>
2021-05-12 20:38:03 -0500

    Example with primitive data types

--- force-app/main/default/lwc/p2cAlertCmpt/p2cAlertCmpt.js
+++ force-app/main/default/lwc/p2cAlertCmpt/p2cAlertCmpt.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class P2cAlertCmpt extends LightningElement {}
+export default class P2cAlertCmpt extends LightningElement {
+    @api message                //Now this atributte is public and you can call it from the cmp
+    @api cardHeading
+    @api number
+    @api isValid
+}
