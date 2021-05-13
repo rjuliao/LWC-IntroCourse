3a642f42d435c4c69702b5d6a12e3c3e5d9ca045
Raul Juliao <raulj@uninorte.edu.co>
2021-05-12 20:55:29 -0500

    Example using non-primitive data type (array, objecto and array of objects)

--- force-app/main/default/lwc/p2cCarouselCmpt/p2cCarouselCmpt.js
+++ force-app/main/default/lwc/p2cCarouselCmpt/p2cCarouselCmpt.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
+import { api, LightningElement } from 'lwc';
 
-export default class P2cCarouselCmpt extends LightningElement {}
+export default class P2cCarouselCmpt extends LightningElement {
+    @api carouselDetails
+}
--- force-app/main/default/lwc/p2cParentCmpt/p2cParentCmpt.js
+++ force-app/main/default/lwc/p2cParentCmpt/p2cParentCmpt.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
 
-export default class P2cParentCmpt extends LightningElement {}
+export default class P2cParentCmpt extends LightningElement {
+    carouselData=[
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-01.jpg",
+            header : "First Card",
+            description : "First card description.",
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-02.jpg",
+            header : "Second Card",
+            description : "Second card description.",
+        },
+        {
+            src : "https://www.lightningdesignsystem.com/assets/images/carousel/carousel-03.jpg",
+            header : "Third Card",
+            description : "Third card description.",
+        }
+    ]
+
+}
