90db869f831f0ca322789891d6b19f8e56ad2b07
Raul Juliao <raulj@uninorte.edu.co>
2021-05-13 15:09:43 -0500

    Example passing data through the event from child to parent

--- force-app/main/default/lwc/c2pModalCmpt/c2pModalCmpt.js
+++ force-app/main/default/lwc/c2pModalCmpt/c2pModalCmpt.js
@@ -4,7 +4,12 @@ export default class C2pModalCmpt extends LightningElement {
 
     //Simple event
     closeHandler(){
-        const myEvent = new CustomEvent('close')
+        const myEvent = new CustomEvent('close',{
+            detail : {
+                msg : "This is the new message pvteria",
+                n : 2021
+            }
+        })
         this.dispatchEvent(myEvent)
     }
 }
--- force-app/main/default/lwc/c2pParentCmpt/c2pParentCmpt.js
+++ force-app/main/default/lwc/c2pParentCmpt/c2pParentCmpt.js
@@ -2,12 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class C2pParentCmpt extends LightningElement {
     showModal = false;
-
+    msg
+    n
+    
     clickHandler(){
         this.showModal = true;
     }
 
-    closeHanlder(){
+    closeHanlder(event){
+        this.msg = event.detail.msg;
+        this.n = event.detail.n;
         this.showModal = false
     }
 }
