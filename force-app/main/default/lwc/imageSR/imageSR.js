8a4bb4ac6f9f1b5946fd20269ca00fe953f942a4
rjuliao <raulj@uninorte.edu.co>
2021-05-28 17:05:59 -0500

    Example for images

--- force-app/main/default/lwc/imageSR/imageSR.js
+++ force-app/main/default/lwc/imageSR/imageSR.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import THE_CROWN_LOGO from '@salesforce/resourceUrl/the_crown';
+import LOGO from '@salesforce/resourceUrl/logo_svg';
 
-export default class ImageSR extends LightningElement {}
+export default class ImageSR extends LightningElement {
+    the_crown = THE_CROWN_LOGO
+    logo_svg_example = LOGO
+}
