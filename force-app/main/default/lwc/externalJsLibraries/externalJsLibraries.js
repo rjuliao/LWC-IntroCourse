411c9a9c2a7054adf01db158d4a7e68510d9757e
rjuliao <raulj@uninorte.edu.co>
2021-05-28 17:41:22 -0500

    Example using moment.js library

--- force-app/main/default/lwc/externalJsLibraries/externalJsLibraries.js
+++ force-app/main/default/lwc/externalJsLibraries/externalJsLibraries.js
@@ -1,3 +1,31 @@
 import { LightningElement } from 'lwc';
+import  MOMENT  from '@salesforce/resourceUrl/moment_library';
+import {loadScript} from 'lightning/platformResourceLoader'
+export default class ExternalJsLibraries extends LightningElement {
+    currentDate
+    isLoadedLib = false
+    renderedCallback(){
+        if(this.isLoadedLib){
+            return
+        }else{
+            loadScript(this, MOMENT+'/moment/moment.min.js')
+            .then(()=>{
+                //Success code :D
+                //call the function
+                this.setActualDate()
+                this.isLoadedLib = true
+            }).catch(error =>{
+                console.log(error);
+            }) 
+        }   
+    }
 
-export default class ExternalJsLibraries extends LightningElement {}
+    setActualDate(){
+        this.currentDate = moment().format('LLLL');
+    }
+
+    handleBtn(){
+        this.setActualDate()
+    }
+
+}
