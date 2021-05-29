16b0f379625dbb4d3072b7936a7cd4dc51759b81
rjuliao <raulj@uninorte.edu.co>
2021-05-29 10:43:19 -0500

    Example of upload a CSS library to make visual movements

--- force-app/main/default/lwc/externalJsLibraries/externalJsLibraries.js
+++ force-app/main/default/lwc/externalJsLibraries/externalJsLibraries.js
@@ -1,6 +1,8 @@
 import { LightningElement } from 'lwc';
-import  MOMENT  from '@salesforce/resourceUrl/moment_library';
-import {loadScript} from 'lightning/platformResourceLoader'
+import MOMENT from '@salesforce/resourceUrl/moment_library';
+import ANIMATE from "@salesforce/resourceUrl/animate_css_lib";
+import {loadScript, loadStyle} from 'lightning/platformResourceLoader'
+
 export default class ExternalJsLibraries extends LightningElement {
     currentDate
     isLoadedLib = false
@@ -8,7 +10,12 @@ export default class ExternalJsLibraries extends LightningElement {
         if(this.isLoadedLib){
             return
         }else{
-            loadScript(this, MOMENT+'/moment/moment.min.js')
+            //This is a promise to make multiple uploads of an entire libraries
+            //In this example we can find a css lib and a js lib
+            Promise.all([
+                loadStyle(this, ANIMATE+'/animate/animate.min.css'),
+                loadScript(this, MOMENT+'/moment/moment.min.js')
+            ])
             .then(()=>{
                 //Success code :D
                 //call the function
@@ -29,3 +36,16 @@ export default class ExternalJsLibraries extends LightningElement {
     }
 
 }
+
+/*
+    This is an example of a single upload of a library
+    loadScript(this, MOMENT+'/moment/moment.min.js')
+    .then(()=>{
+        //Success code :D
+        //call the function
+        this.setActualDate()
+        this.isLoadedLib = true
+    }).catch(error =>{
+        console.log(error);
+    }) 
+*/
