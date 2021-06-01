918b17027054ff339690c7a3403772c0a917fadd
rjuliao <raulj@uninorte.edu.co>
2021-06-01 10:31:08 -0500

    Notification example using toast

--- force-app/main/default/lwc/notifications/notifications.js
+++ force-app/main/default/lwc/notifications/notifications.js
@@ -1,3 +1,30 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 
-export default class Notifications extends LightningElement {}
+export default class Notifications extends LightningElement {
+
+    handleClickOne(){
+        this.showToast('SUCCESS!!','Viva la pvteria','success')
+    }
+
+    handleClickTwo(){
+        this.showToast('ERROR!!','Muerte a la pvteria','error')
+    }
+
+    handleClickThree(){
+        this.showToast('WARNING!!','Ojo con la pvteria','warning')
+    }
+
+    handleClickFour(){
+        this.showToast('INFORMATION!!','Recuerda la pvteria','info')
+    }
+
+    showToast(title, message, variant){
+        const event = new ShowToastEvent({
+            title,
+            message,
+            variant
+        });
+        this.dispatchEvent(event);
+    }
+}
