bb6ddce44e78ac74b5a116358153320aae1a0c96
rjuliao <raulj@uninorte.edu.co>
2021-07-06 09:38:50 -0500

    Example of wire property and function

--- force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
+++ force-app/main/default/lwc/wireDemoUserDetail/wireDemoUserDetail.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import Id from '@salesforce/user/Id'
+import { getRecord } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/User.Name'
+import EMAIL_FIELD from '@salesforce/schema/User.Email'
 
-export default class WireDemoUserDetail extends LightningElement {}
+const fields = [NAME_FIELD, EMAIL_FIELD]
+
+export default class WireDemoUserDetail extends LightningElement {
+    userId = Id
+    userDetails
+    /**
+     * @wire(adapter, {adapterConfig})
+     * propertyOrFunction
+     */
+
+    //recommended
+    @wire(getRecord, {recordId : Id, fields : fields})
+    userDetailHandler({data,error}){
+        if (data) {
+            this.userDetails = data.fields
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+
+    @wire(getRecord, {recordId : '$userId', fields : fields})
+    userDetailProperty
+}
