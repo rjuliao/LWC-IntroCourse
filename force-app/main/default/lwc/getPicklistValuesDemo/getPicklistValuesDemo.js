0d15af451858120ca684c5031f2724c95c719ccd
rjuliao <raulj@uninorte.edu.co>
2021-07-06 14:01:13 -0500

    demo

--- force-app/main/default/lwc/getPicklistValuesDemo/getPicklistValuesDemo.js
+++ force-app/main/default/lwc/getPicklistValuesDemo/getPicklistValuesDemo.js
@@ -1,3 +1,31 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry'
+import ACCOUNT from '@salesforce/schema/Account'
+import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
 
-export default class GetPicklistValuesDemo extends LightningElement {}
+export default class GetPicklistValuesDemo extends LightningElement {
+    selectedIndustry = ''
+    industryOptions = []
+
+    @wire(getObjectInfo, {objectApiName : ACCOUNT})
+    objectInfo
+
+    @wire(getPicklistValues, {recordTypeId : '$objectInfo.data.defaultRecordTypeId' , fieldApiName : INDUSTRY_FIELD})
+    industryPicklistHandler({data, error}){
+        if(data){
+            console.log(data);
+            this.industryOptions = [...this.generatePicklist(data)]
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+
+    generatePicklist(data){
+        return data.values.map(item =>({label : item.label, value : item.value}))
+    }
+
+    handleChange(event){
+        this.selectedIndustry = event.detail.value
+    }
+}
