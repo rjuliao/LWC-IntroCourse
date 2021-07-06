9a3e5ab633f47d227788141bdcb3810d1604ae8d
rjuliao <raulj@uninorte.edu.co>
2021-07-06 10:56:20 -0500

    objectInfos demo

--- force-app/main/default/lwc/getObjectInfoDemo/getObjectInfoDemo.js
+++ force-app/main/default/lwc/getObjectInfoDemo/getObjectInfoDemo.js
@@ -1,3 +1,33 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account'
+import OPPORTUNITY_OBJECT from '@salesforce/schema/Opportunity'
+import { getObjectInfo, getObjectInfos } from 'lightning/uiObjectInfoApi';
 
-export default class GetObjectInfoDemo extends LightningElement {}
+export default class GetObjectInfoDemo extends LightningElement {
+    objectInfo
+    objectInfos
+
+
+    @wire(getObjectInfo, {objectApiName : ACCOUNT_OBJECT})
+    objectInfoHandler({data, error}){
+        if(data){
+            console.log(data);
+            this.objectInfo = data
+        }
+        if(error){
+            console.error(error);
+        }
+    }
+
+    objectApiNames = [ACCOUNT_OBJECT, OPPORTUNITY_OBJECT]
+    @wire(getObjectInfos, {objectApiNames : '$objectApiNames'})
+    objectInfosHandler({data, error}){
+        if (data) {
+            console.log('Infors', data);
+            this.objectInfos = data
+        }
+        if (error) {
+            console.error(error);
+        }
+    }
+}
