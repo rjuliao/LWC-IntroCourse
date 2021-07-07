648223e1e5405bdf9994cade94271c2b4af6b38a
rjuliao <raulj@uninorte.edu.co>
2021-07-07 11:19:14 -0500

    createRecordDemo

--- force-app/main/default/lwc/createRecordDemo/createRecordDemo.js
+++ force-app/main/default/lwc/createRecordDemo/createRecordDemo.js
@@ -1,3 +1,36 @@
 import { LightningElement } from 'lwc';
+import CONTACT_OBJECT from '@salesforce/schema/Contact'
+import { createRecord } from 'lightning/uiRecordApi';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
 
-export default class CreateRecordDemo extends LightningElement {}
+export default class CreateRecordDemo extends LightningElement {
+    formFields = {}
+
+    changeHandler(event){
+        const {name, value} = event.target
+        this.formFields[name] = value
+    }
+
+    createContact(){
+        const recordInput = {apiName : CONTACT_OBJECT.objectApiName, fields : this.formFields}
+        createRecord(recordInput)
+        .then(res =>{
+            console.log(res);
+            this.showToast("Record successfully created", `Contact created with id ${res.id}`)
+            this.template.querySelector('form.createForm').reset()
+            this.formFields = {}
+        })
+        .catch(err=>{
+            console.error(err);
+            this.showToast("Error creating Record", error.body.message, 'error')
+        })
+    }
+
+    showToast(title, message, variant){
+        this.dispatchEvent(new ShowToastEvent({
+            title,
+            message,
+            variant : variant || 'success'
+        }))
+    }
+}
